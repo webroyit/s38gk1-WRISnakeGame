e3ceceec31e14dc4482c56f858eb5787ec5e4d51
royweb <xytank@gmail.com>
2020-01-22 12:58:31 -0500

    Set up variables

--- snake.js
+++ snake.js
@@ -10,3 +10,18 @@ background.src = "img/background.png";
 
 const foodImg = new Image();
 foodImg.src = "img/food.png";
+
+// create the snake
+let snake = [];
+snake[0] = {
+    x: 9 * box;
+    y: 10 * box;
+}
+
+// create the food
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+}
+
+let score = 0;
