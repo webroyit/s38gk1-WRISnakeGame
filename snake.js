779c1396cbdc9824eb3657f387d2adc4a6d4582d
royweb <xytank@gmail.com>
2020-01-22 12:52:57 -0500

    Add images

--- snake.js
+++ snake.js
@@ -1,4 +1,12 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
+// size of each space
 const box = 32;
+
+// load the images
+const background = new Image();
+background.src = "img/background.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
